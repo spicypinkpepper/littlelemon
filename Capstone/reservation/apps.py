7652ccf8afa17c0b263ba88ffcafae08ac9b1dd3
Lionel Jensen <jensen.lionel@gmail.com>
2023-10-17 12:30:23 -0400

    third commit

--- Capstone/reservation/apps.py
+++ Capstone/reservation/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CapstoneappConfig(AppConfig):
+class ReservationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'capstoneapp'
+    name = 'reservation'
