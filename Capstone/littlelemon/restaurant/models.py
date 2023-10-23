1f5de82e4be15ceab6d204fd5fb0be67db70b282
Lionel Jensen <jensen.lionel@gmail.com>
2023-10-23 11:54:33 -0400

    added models and mysql setup 2

--- Capstone/littlelemon/restaurant/models.py
+++ Capstone/littlelemon/restaurant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+from django.utils import timezone
+
 
 # Create your models here.
+class Booking(models.Model):
+    Name = models.CharField(max_length = 255)
+    No_of_guests = models.IntegerField(default = 1)
+    BookingDate = models.DateTimeField(default = timezone.now)
+    
+class Menu(models.Model):
+    Title = models.CharField(max_length = 255)
+    Price = models.DecimalField(max_digits = 10,decimal_places=2)
+    Inventory = models.IntegerField(default = 1)
+    
